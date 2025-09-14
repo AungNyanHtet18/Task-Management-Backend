0eefb1fb3d1fb3d95a28babd220ff379b1230b26
AungNyanHtet18 <aungnyanh81@gmail.com>
2025-09-14 12:20:01 +0630

    Commit Message

--- src/main/java/com/dev/anh/task/api/input/ProjectForm.java
+++ src/main/java/com/dev/anh/task/api/input/ProjectForm.java
@@ -2,11 +2,31 @@ package com.dev.anh.task.api.input;
 
 import java.time.LocalDate;
 
+import com.dev.anh.task.model.entity.Project;
+
+import jakarta.validation.constraints.NotBlank;
+
 public record ProjectForm(
+	@NotBlank
 	String name,
+	
 	LocalDate startDate,
 	LocalDate dueDate,
 	String description
 	) {
+
+	public Project entity() {
+		var entity = new Project();
+		update(entity);
+		return entity;
+	}
+
+	public void update(Project entity) {
+		entity.setName(name);
+		entity.setStartDate(startDate);
+		entity.setDueDate(dueDate);
+		entity.setDescription(description);
+	}
+	
 
 }
--- src/main/java/com/dev/anh/task/service/ProjectService.java
+++ src/main/java/com/dev/anh/task/service/ProjectService.java
@@ -2,25 +2,38 @@ package com.dev.anh.task.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dev.anh.task.api.input.ProjectForm;
 import com.dev.anh.task.api.input.ProjectSearch;
 import com.dev.anh.task.api.output.ModificationResult;
 import com.dev.anh.task.api.output.ProjectDetails;
 import com.dev.anh.task.api.output.ProjectListItem;
+import com.dev.anh.task.model.repo.ProjectRepo;
+
 
 @Service
+@Transactional(readOnly = true) //for public method /if i want to override,i set in method level(@Transactional) 
 public class ProjectService {
 
+	@Autowired
+	private ProjectRepo repo;
+	
+	@Transactional
 	public ModificationResult<Integer> create(ProjectForm form) {
-		
-		return null;
+		var entity =  repo.save(form.entity()); //returned entity is in the  managed state
+		return ModificationResult.success(entity.getId());
 	}
-
+	
+	
+	@Transactional
 	public ModificationResult<Integer> update(int id, ProjectForm form) {
+		var entity = repo.findById(id).orElseThrow();
+		form.update(entity);
 		
-		return null;
+		return ModificationResult.success(entity.getId());
 	}
 	
 	
